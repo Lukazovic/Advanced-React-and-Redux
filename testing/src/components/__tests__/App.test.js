e93688341639eb17a3d7e9674a1acd0f118348e1
Lucas <lucas@mint-19.3>
2020-05-21 11:26:23 -0300

    write the first test but in a wrong way

--- testing/src/components/__tests__/App.test.js
+++ testing/src/components/__tests__/App.test.js
@@ -3,9 +3,11 @@ import ReactDOM from "react-dom";
 import App from "../App";
 
 it("shows a comment box", () => {
-  const div = document.create("div");
+  const div = document.createElement("div");
 
   ReactDOM.render(<App />, div);
+
+  expect(div.innerHTML).toContain("Comment Box");
 
   ReactDOM.unmountComponentAtNode(div);
 });
