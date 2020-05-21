8a79e81af424929c47baa7addbffad9bfb90a857
Lucas <lucas@mint-19.3>
2020-05-21 13:12:06 -0300

    Add beforeEach to reuse code in App.test

--- testing/src/components/__tests__/App.test.js
+++ testing/src/components/__tests__/App.test.js
@@ -4,14 +4,16 @@ import App from "../App";
 import CommentBox from "../CommentBox";
 import CommentList from "../CommentList";
 
-it("shows a comment box", () => {
-  const wrapped = shallow(<App />);
+let wrapped;
 
+beforeEach(() => {
+  wrapped = shallow(<App />);
+});
+
+it("shows a comment box", () => {
   expect(wrapped.find(CommentBox).length).toEqual(1);
 });
 
 it("show a comment list", () => {
-  const wrapped = shallow(<App />);
-
   expect(wrapped.find(CommentList).length).toEqual(1);
 });
