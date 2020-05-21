5a5f44194931f97a9974f89b16b8542989954f27
Lucas <lucas@mint-19.3>
2020-05-21 15:05:26 -0300

    Add beforeEach and afterEach to unmount components

--- testing/src/components/__tests__/CommentBox.test.js
+++ testing/src/components/__tests__/CommentBox.test.js
@@ -2,9 +2,17 @@ import React from "react";
 import { mount } from "enzyme";
 import CommentBox from "../CommentBox";
 
-it("has a comment area and a button", () => {
-  const wrapped = mount(<CommentBox />);
+let wrapped;
 
+beforeEach(() => {
+  wrapped = mount(<CommentBox />);
+});
+
+afterEach(() => {
+  wrapped.unmount();
+});
+
+it("has a comment area and a button", () => {
   expect(wrapped.find("textarea").length).toEqual(1);
   expect(wrapped.find("button").length).toEqual(1);
 });
