29313174db07e3b8a4b328f30de21722e103ed91
Lucas <lucas@mint-19.3>
2020-05-22 14:59:21 -0300

    Refactoring Root Component to accept initialState

--- testing/src/Root.js
+++ testing/src/Root.js
@@ -3,8 +3,10 @@ import { Provider } from "react-redux";
 import { createStore } from "redux";
 import reducers from "./reducers";
 
-export default props => {
+const Root = ({ children, initialState = {} }) => {
   return (
-    <Provider store={createStore(reducers, {})}>{props.children}</Provider>
+    <Provider store={createStore(reducers, initialState)}>{children}</Provider>
   );
 };
+
+export default Root;
