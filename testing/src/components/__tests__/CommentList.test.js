ed698392cf27e2114a037d1cbf256bfc49eb71b9
Lucas <lucas@mint-19.3>
2020-05-22 15:08:42 -0300

    Add test to check if the componnent is rendering one LI per Comment

---
+++ testing/src/components/__tests__/CommentList.test.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { mount } from "enzyme";
+
+import CommentList from "../CommentList";
+import Root from "../../Root";
+
+let wrapped;
+
+beforeEach(() => {
+  const initialState = {
+    comments: ["Comment 1", "Comment 2"],
+  };
+
+  wrapped = mount(
+    <Root initialState={initialState}>
+      <CommentList />
+    </Root>
+  );
+});
+
+it("create one LI per comment", () => {
+  expect(wrapped.find("li").length).toEqual(2);
+});
