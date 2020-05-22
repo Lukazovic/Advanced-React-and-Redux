ee8611b8599fd88e0d5bff7e9884c17985a36c8d
Lucas <lucas@mint-19.3>
2020-05-22 09:59:05 -0300

    Add test to check if the user can type in textarea

--- testing/src/components/__tests__/CommentBox.test.js
+++ testing/src/components/__tests__/CommentBox.test.js
@@ -12,7 +12,16 @@ afterEach(() => {
   wrapped.unmount();
 });
 
-it("has a comment area and a button", () => {
+it("has a text area and a button", () => {
   expect(wrapped.find("textarea").length).toEqual(1);
   expect(wrapped.find("button").length).toEqual(1);
+});
+
+it("has a text area that users can type in", () => {
+  wrapped.find("textarea").simulate("change", {
+    target: { value: "new comment" },
+  });
+  wrapped.update();
+
+  expect(wrapped.find("textarea").prop("value")).toEqual("new comment");
 });
