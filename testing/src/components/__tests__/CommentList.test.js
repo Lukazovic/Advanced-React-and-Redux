f4037f1c3af93ed63a969b5cfb31c9eb6f814f82
Lucas <lucas@mint-19.3>
2020-05-22 15:15:14 -0300

    Add Cheerio Queries to test if texts are renderings

--- testing/src/components/__tests__/CommentList.test.js
+++ testing/src/components/__tests__/CommentList.test.js
@@ -21,3 +21,8 @@ beforeEach(() => {
 it("create one LI per comment", () => {
   expect(wrapped.find("li").length).toEqual(2);
 });
+
+it("show the text for each comment", () => {
+  expect(wrapped.render().text()).toContain("Comment 1");
+  expect(wrapped.render().text()).toContain("Comment 2");
+});
