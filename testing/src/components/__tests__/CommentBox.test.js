100200e6148a9d11a40d95395547da26d5ac8e3e
Lucas <lucas@mint-19.3>
2020-05-22 10:22:27 -0300

    Group testes that have the same codes

--- testing/src/components/__tests__/CommentBox.test.js
+++ testing/src/components/__tests__/CommentBox.test.js
@@ -20,23 +20,22 @@ it("has a button", () => {
   expect(wrapped.find("button").length).toEqual(1);
 });
 
-it("has a text area that users can type in", () => {
-  wrapped.find("textarea").simulate("change", {
-    target: { value: "new comment" },
+describe("the text area", () => {
+  beforeEach(() => {
+    wrapped.find("textarea").simulate("change", {
+      target: { value: "new comment" },
+    });
+    wrapped.update();
   });
-  wrapped.update();
 
-  expect(wrapped.find("textarea").prop("value")).toEqual("new comment");
-});
-
-it("clears the text area after a submit", () => {
-  wrapped.find("textarea").simulate("change", {
-    target: { value: "new comment" },
+  it("has a text area that users can type in", () => {
+    expect(wrapped.find("textarea").prop("value")).toEqual("new comment");
   });
-  wrapped.update();
 
-  wrapped.find("form").simulate("submit");
-  wrapped.update();
+  it("clears the text area after a submit", () => {
+    wrapped.find("form").simulate("submit");
+    wrapped.update();
 
-  expect(wrapped.find("textarea").prop("value")).toEqual("");
+    expect(wrapped.find("textarea").prop("value")).toEqual("");
+  });
 });
