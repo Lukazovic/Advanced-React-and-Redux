6a96c84b450435e3d69e526d31ca8f7162ed4a62
Lucas <lucas@mint-19.3>
2020-05-23 12:42:50 -0300

    Add a navBar

--- higher-order-components/src/components/App.js
+++ higher-order-components/src/components/App.js
@@ -1,13 +1,35 @@
 import React from "react";
-import { Route } from "react-router-dom";
+import { Route, Link } from "react-router-dom";
+import { connect } from "react-redux";
 import CommentBox from "./CommentBox";
 import CommentList from "./CommentList";
 
-export default () => {
+const App = ({ auth }) => {
+  const signButton = <button>{auth ? "Sign out" : "Sign in"}</button>;
+
+  const navBar = (
+    <ul>
+      <li>
+        <Link to="/">Home</Link>
+      </li>
+      <li>
+        <Link to="/post">Post a comment</Link>
+      </li>
+      <li>{signButton}</li>
+    </ul>
+  );
+
   return (
     <div>
+      {navBar}
       <Route exact path="/post" component={CommentBox} />
       <Route exact path="/" component={CommentList} />
     </div>
   );
 };
+
+const mapStateToProps = state => ({
+  auth: state.auth,
+});
+
+export default connect(mapStateToProps)(App);
