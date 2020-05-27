53cfab132abbae1dea467e153194235c2313273e
Lukazovic <lucas10.vieira.lv@gmail.com>
2020-05-27 19:13:52 -0300

    Add init route file

--- Authentication/server/src/index.js
+++ Authentication/server/src/index.js
@@ -3,10 +3,12 @@ const http = require("http");
 const bodyParser = require("body-parser");
 const morgan = require("morgan");
 const app = express();
+const router = require("./router");
 
 // App setup
 app.use(morgan("combined"));
 app.use(bodyParser.json({ type: "*/*" }));
+app.use(router);
 
 // Server setup
 const port = process.env.PORT || 3333;
