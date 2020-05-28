2640a8f30f8e8d9f7ecf0ec3c819065ca215e4b9
Lukazovic <lucas10.vieira.lv@gmail.com>
2020-05-28 16:58:53 -0300

    Add a Feature Component

---
+++ Authentication/client/src/components/Feature.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Feature() {
+  return <div>This is the feature</div>;
+}
+
+export default Feature;
--- Authentication/client/src/index.js
+++ Authentication/client/src/index.js
@@ -9,6 +9,7 @@ import reducers from "./components/reducers";
 import App from "./components/App";
 import Welcome from "./components/Welcome";
 import Signup from "./components/auth/Signup";
+import Feature from "./components/Feature";
 
 const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
 
@@ -19,6 +20,7 @@ ReactDOM.render(
         <App>
           <Route exact path="/" component={Welcome} />
           <Route exact path="/signup" component={Signup} />
+          <Route exact path="/feature" component={Feature} />
         </App>
       </BrowserRouter>
     </Provider>
