8c79fc918aacf69e353adb4d0ba24f8f587f77b4
Lukazovic <lucas10.vieira.lv@gmail.com>
2020-05-27 22:49:07 -0300

    setup MongoDB connection

--- Authentication/server/src/index.js
+++ Authentication/server/src/index.js
@@ -4,6 +4,14 @@ const bodyParser = require("body-parser");
 const morgan = require("morgan");
 const app = express();
 const router = require("./router");
+const mongoose = require("mongoose");
+
+// DB setup
+mongoose.connect("mongodb://127.0.0.1:27017/auth", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+});
 
 // App setup
 app.use(morgan("combined"));
