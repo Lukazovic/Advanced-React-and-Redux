2587ba87d52531c6e25334c337e1cda9f06fc4f0
Lukazovic <lucas10.vieira.lv@gmail.com>
2020-05-28 17:35:20 -0300

    Add signin action

--- Authentication/client/src/actions/index.js
+++ Authentication/client/src/actions/index.js
@@ -16,6 +16,21 @@ export const signup = (formProps, callback) => async dispatch => {
   }
 };
 
+export const signin = (formProps, callback) => async dispatch => {
+  try {
+    const response = await axios.post(
+      "http://localhost:3333/signin",
+      formProps
+    );
+
+    dispatch({ type: AUTH_USER, payload: response.data.token });
+    localStorage.setItem("token", response.data.token);
+    callback();
+  } catch (err) {
+    dispatch({ type: AUTH_ERROR, payload: "Invalid login credentials" });
+  }
+};
+
 export const signout = () => {
   localStorage.removeItem("token");
 
