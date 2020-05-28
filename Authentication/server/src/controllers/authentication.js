2e96675ae6c49a6e62a610612348b4a01938b3d1
Lukazovic <lucas10.vieira.lv@gmail.com>
2020-05-27 23:22:07 -0300

    Creating user Records

--- Authentication/server/src/controllers/authentication.js
+++ Authentication/server/src/controllers/authentication.js
@@ -1,5 +1,23 @@
+const User = require("../models/user");
+
 module.exports = {
-  signup(req, res) {
-    res.send({ sucess: true });
+  async signup(req, res, next) {
+    const { email, password } = req.body;
+
+    try {
+      // Verify if the email is in use
+      const user = await User.findOne({ email });
+
+      // If a user with email does exist, return an error
+      if (user) return res.status(422).send({ error: "Email is in use" });
+
+      // If a user with email does NOT exist, create and save user record
+      const newUser = new User({ email, password });
+      await newUser.save();
+
+      res.json({ success: true });
+    } catch (err) {
+      return next(err);
+    }
   },
 };
